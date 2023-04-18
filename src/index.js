944fe0237cb2ce003539741cdb5cde612e640484
axtellion <slark19735@gmail.com>
2023-04-18 23:51:21 +0300

    add

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="test_task/">
+    <BrowserRouter basename="/test_task/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
